09e963bb9bd9a057c9802b11fcea58144e3a9b3f
Zane <zaneshaw.home@gmail.com>
2022-06-20 08:38:29 +1000

    AMMEND THIS

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,7 @@
 const functions = require("firebase-functions");
+const admin = require('firebase-admin');
+
+admin.initializeApp(functions.config().firebase);
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -7,3 +10,26 @@ const functions = require("firebase-functions");
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+exports.createUser = functions.auth.user().onCreate(() => {
+    console.log("+1");
+    const increment = admin.firestore.FieldValue.increment(1);
+
+    var ref = admin.firestore().collection("var").doc("stats");
+    ref.update({
+        usersCreated: increment,
+        users: increment
+    });
+
+    return null;
+});
+
+exports.deleteUser = functions.auth.user().onDelete(() => {
+    console.log("-1");
+    const decrement = admin.firestore.FieldValue.increment(-1);
+
+    var ref = admin.firestore().collection("var").doc("stats");
+    ref.update({ users: decrement });
+
+    return null;
+});
