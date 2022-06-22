f975384db144c4728a04e7f35ead8bae7041b511
Zane <zaneshaw.home@gmail.com>
2022-06-22 16:36:07 +1000

    Change function region to southeast Australia

--- functions/index.js
+++ functions/index.js
@@ -6,39 +6,43 @@ admin.initializeApp(functions.config().firebase);
 const increment = admin.firestore.FieldValue.increment(1);
 const decrement = admin.firestore.FieldValue.increment(-1);
 
-exports.createUser = functions.auth.user().onCreate((user) => {
-    console.log("Creating user...");
+exports.createUser = functions
+    .region("australia-southeast1")
+    .auth.user().onCreate((user) => {
+        console.log("Creating user...");
 
-    // Set user document
-    const userRef = admin.firestore().collection("users").doc(user.uid);
-    userRef.set({
-        createdAt: user.metadata.creationTime,
-        displayName: user.providerData[0].screenName, 
-        email: user.email,
+        // Set user document
+        const userRef = admin.firestore().collection("users").doc(user.uid);
+        userRef.set({
+            createdAt: user.metadata.creationTime,
+            displayName: user.providerData[0].screenName,
+            email: user.email,
+        });
+
+        // Update stats
+        const statsRef = admin.firestore().collection("_var").doc("stats");
+        statsRef.set({
+            usersCreated: increment,
+            users: increment
+        }, { merge: true });
+
+        console.log("User created!");
+        return null;
     });
-    
-    // Update stats
-    const statsRef = admin.firestore().collection("_var").doc("stats");
-    statsRef.set({
-        usersCreated: increment,
-        users: increment
-    }, { merge: true });
 
-    console.log("User created!");
-    return null;
-});
+exports.deleteUser = functions
+    .region("australia-southeast1")
+    .auth.user().onDelete((user) => {
+        console.log("Deleting user...");
 
-exports.deleteUser = functions.auth.user().onDelete((user) => {
-    console.log("Deleting user...");
+        // Remove user document
+        const userRef = admin.firestore().collection("users").doc(user.uid);
+        userRef.delete();
 
-    // Remove user document
-    const userRef = admin.firestore().collection("users").doc(user.uid);
-    userRef.delete();
-    
-    // Update stats
-    const ref = admin.firestore().collection("_var").doc("stats");
-    ref.set({ users: decrement }, { merge: true });
+        // Update stats
+        const ref = admin.firestore().collection("_var").doc("stats");
+        ref.set({ users: decrement }, { merge: true });
 
-    console.log("User deleted!");
-    return null;
-});
+        console.log("User deleted!");
+        return null;
+    });
