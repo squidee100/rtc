c07defb7974385bde13c7f9988b354b9a0a7df8b
Zane <zaneshaw.home@gmail.com>
2022-06-24 08:30:31 +1000

    Update timesince.js

--- src/utils/timesince.js
+++ src/utils/timesince.js
@@ -4,24 +4,32 @@ export function timeSince(date) {
 
     var interval = seconds / 31536000;
 
-    if (interval > 1) {
-        str = Math.floor(interval) + " year";
-    }
-    interval = seconds / 2592000;
-    if (interval > 1) {
-        str = Math.floor(interval) + " month";
-    }
-    interval = seconds / 86400;
-    if (interval > 1) {
-        str = Math.floor(interval) + " day";
-    }
-    interval = seconds / 3600;
-    if (interval > 1) {
-        str = Math.floor(interval) + " hour";
-    }
-    interval = seconds / 60;
-    if (interval > 1) {
-        str = Math.floor(interval) + " minute";
+    while (true) {
+        if (interval > 1) {
+            str = Math.floor(interval) + " year";
+            break;
+        }
+        interval = seconds / 2592000;
+        if (interval > 1) {
+            str = Math.floor(interval) + " month";
+            break;
+        }
+        interval = seconds / 86400;
+        if (interval > 1) {
+            str = Math.floor(interval) + " day";
+            break;
+        }
+        interval = seconds / 3600;
+        if (interval > 1) {
+            str = Math.floor(interval) + " hour";
+            break;
+        }
+        interval = seconds / 60;
+        if (interval > 1) {
+            str = Math.floor(interval) + " minute";
+            break;
+        }
+        break;
     }
 
     if (interval >= 2) {
