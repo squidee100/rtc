8ad9f3d30e0a371e365193eb9cc7763560694987
Zane <zaneshaw.home@gmail.com>
2022-06-23 19:24:57 +1000

    Remove create-react-app template code

--- src/App.js
+++ src/App.js
@@ -1,28 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
-import * as FirebaseService from "./services/firebase";
 import { useEffect, useState } from 'react';
 
 function App() {
 	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-
-				<button onClick={FirebaseService.SignIn}>Sign In</button>
-				<UserList userList={FirebaseService.userList} />
-			</header>
+		<div className="app">
 		</div>
 	);
 }
